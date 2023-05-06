525a3b056e024b86ab3e9febb060491c970a43db
mquarne <mquarne@nellisauction.com>
2023-05-06 13:59:52 -0700

    added loader to analysis and raw page

--- app/routes/_app.expenses_.analysis.tsx
+++ app/routes/_app.expenses_.analysis.tsx
@@ -1,26 +1,59 @@
 import Chart from "~/components/expenses/Chart";
 import ExpenseStatistics from "~/components/expenses/ExpenseStatistics";
+import {
+  isRouteErrorResponse,
+  Link,
+  useLoaderData,
+  useRouteError,
+} from "@remix-run/react";
+import { getExpenses } from "~/data/expenses.server";
+import { json } from "@remix-run/node";
+import ErrorComponent from "~/components/util/ErrorComponent";
 
-const DummyExpenses = [
-  {
-    id: "e1",
-    title: "First Expense",
-    amount: 12.99,
-    date: new Date().toISOString(),
-  },
-  {
-    id: "e1",
-    title: "Second Expense",
-    amount: 16.99,
-    date: new Date().toISOString(),
-  },
-];
+export async function loader() {
+  const expenses = await getExpenses();
+
+  if (!expenses || expenses.length === 0) {
+    throw json(
+      { message: "Could not load expenses for analysis" },
+      {
+        status: 404,
+        statusText: "Expenses not found",
+      }
+    );
+  }
+
+  return json(expenses);
+}
 
 export default function AnalysisPage() {
+  const expenses = useLoaderData();
+
   return (
     <main>
-      <Chart expenses={DummyExpenses} />
-      <ExpenseStatistics expenses={DummyExpenses} />
+      <Chart expenses={expenses} />
+      <ExpenseStatistics expenses={expenses} />
     </main>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  // when true, this is what used to go to `CatchBoundary`
+  if (isRouteErrorResponse(error)) {
+    return (
+      <main>
+        <ErrorComponent title={error.statusText}>
+          <p>
+            {error.data?.message ||
+              "Something went wrong with expense analysis"}
+          </p>
+          <p>
+            Back To <Link to={"/expenses"}>Expenses</Link>
+          </p>
+        </ErrorComponent>
+      </main>
+    );
+  }
 }
